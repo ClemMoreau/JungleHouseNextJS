4e51f56c1a21d23dbb2f2f4d2474c8875e87db6e
Clément <clement@Mac-mini-dAlexandre.local>
2022-07-07 10:15:52 +0200

    Eslint + Prettier setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,31 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import { useState, useEffect } from 'react'
-import logo from '../public/logo.png'
-import styles from '../styles/Home.module.css'
-import NavBar from '../components/navbar'
+import type { NextPage } from "next";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import logo from "../public/logo.png";
+import styles from "../styles/Home.module.css";
+import NavBar from "../components/navbar";
 
 const Home: NextPage = () => {
+  const [cart, updateCart] = useState([]);
 
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    updateCart(savedCart ? JSON.parse(savedCart) : []);
+  });
 
-  // const savedCart = localStorage.getItem('cart');
-  // const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : []);
-  // useEffect(() => {
-  //   localStorage.setItem('cart',JSON.stringify(cart))
-  // }, [cart])
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <div>
       <header className={styles.header}>
-        <div>
-          Jungle House
-        </div>
-        <Image src={logo} alt='jungle-house-logo'/>
+        <div>Jungle House</div>
+        <Image src={logo} alt="jungle-house-logo" />
       </header>
-      <NavBar/>
+      <NavBar />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
